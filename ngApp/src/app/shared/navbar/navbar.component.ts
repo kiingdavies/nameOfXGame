4fd18e35b0731ac9b0a016b3d7518b01dfdc62fb
kiingdavies <daviesadedeji007@gmail.com>
2020-12-17 08:03:14 +0100

    register, login, logout, navbar Done

--- ngApp/src/app/shared/navbar/navbar.component.ts
+++ ngApp/src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../auth.service';
+import { AuthService } from './../../auth.service';
+
 
 @Component({
   selector: 'app-navbar',
@@ -8,19 +9,19 @@ import { AuthService } from '../../auth.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
-  // // this function calls the loggedin method in auth.service
-  // login() {
-  //   return this._authService.loggedIn();
-  // }
+  // this function calls the loggedin method in auth.service
+  login() {
+    return this._authService.loggedIn();
+  }
 
-  // // this function calls the logoutUser method in auth.service
-  // logout() {
-  //   return this._authService.logoutUser();
-  // }
+  // this function calls the logoutUser method in auth.service
+  logout() {
+    return this._authService.logoutUser();
+  }
 
 }
